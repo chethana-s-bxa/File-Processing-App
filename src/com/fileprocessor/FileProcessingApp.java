1afeacbb86b9bdde8367de479d74e03713b7d374
chethana-s-bxa <chethana.s@bounteous.com>
2026-03-09 14:17:45 +0530

    Implemented directory scanning and filtering of txt files with max limit of 100

--- src/com/fileprocessor/FileProcessingApp.java
+++ src/com/fileprocessor/FileProcessingApp.java
@@ -1,9 +1,27 @@
 package com.fileprocessor;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileProcessingApp {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+
+
+        System.out.println("Scanning directory......");
+
         File directory = new File("files");
+        File[] files = directory.listFiles();
+        List<File> textFiles = new ArrayList<>();
+
+        if(files != null){
+            for(File file : files){
+                if(textFiles.size() == 100) break;
+                if(file.isFile() && file.getName().endsWith(".txt")){
+                    textFiles.add(file);
+                }
+            }
+        }
+        System.out.println("Total files found: "+textFiles.size());
     }
 }
