5892c587c187206a1b4c221b4991ae4d63ec08ab
chethana-s-bxa <chethana.s@bounteous.com>
2026-03-09 13:54:34 +0530

    Moved files package outside src

--- src/com/fileprocessor/FileProcessingApp.java
+++ src/com/fileprocessor/FileProcessingApp.java
@@ -1,7 +1,9 @@
 package com.fileprocessor;
 
+import java.io.File;
+
 public class FileProcessingApp {
     static void main(String[] args) {
-
+        File directory = new File("files");
     }
 }
