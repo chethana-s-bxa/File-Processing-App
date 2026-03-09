fbf1ee1d9e753cf48c1b861117edefe201307ce9
chethana-s-bxa <chethana.s@bounteous.com>
2026-03-09 16:49:07 +0530

    Introduced executor service with 4 fixed threads

--- src/com/fileprocessor/FileProcessingApp.java
+++ src/com/fileprocessor/FileProcessingApp.java
@@ -1,8 +1,15 @@
 package com.fileprocessor;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
 
 public class FileProcessingApp {
     public static void main(String[] args) {
@@ -14,7 +21,7 @@ public class FileProcessingApp {
         File directory = new File("files");
         File[] files = directory.listFiles();
         List<File> textFiles = new ArrayList<>();
-
+        List<Future<FileStats>> futures = new ArrayList<>();
         if(files != null){
             for(File file : files){
                 if(textFiles.size() == 100) break;
@@ -24,18 +31,44 @@ public class FileProcessingApp {
             }
         }
 
+
         System.out.println("Total files found: "+textFiles.size()+"\n");
         System.out.println("Processing files......\n");
-
+        ExecutorService executor = Executors.newFixedThreadPool(4);
         for(File file : textFiles){
-            FileStats data = FileProcessor.processFile(file);
-            totalLines += data.getLineCount();
-            totalWords += data.getWordCount();
-            System.out.println("File: " +data.getFileName());
-            System.out.println("Lines: "+data.getLineCount());
-            System.out.println("Words: "+data.getWordCount());
-            System.out.println();
+            FileProcessingTask task = new FileProcessingTask(file);
+            Future<FileStats> future = executor.submit(task);
+            futures.add(future);
         }
+
+//        Sequential
+//        for(File file : textFiles){
+//            FileStats data = FileProcessor.processFile(file);
+//            totalLines += data.getLineCount();
+//            totalWords += data.getWordCount();
+//            System.out.println("File: " +data.getFileName());
+//            System.out.println("Lines: "+data.getLineCount());
+//            System.out.println("Words: "+data.getWordCount());
+//            System.out.println();
+//        }
+
+        for(Future<FileStats> future : futures){
+            try {
+                FileStats data = future.get();
+
+                totalLines += data.getLineCount();
+                totalWords += data.getWordCount();
+
+                System.out.println("File: " + data.getFileName());
+                System.out.println("Lines: " + data.getLineCount());
+                System.out.println("Words: " + data.getWordCount());
+                System.out.println();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
         long endTime = System.currentTimeMillis();
         System.out.println("---------------------------------------------");
         System.out.println("SUMMARY");
